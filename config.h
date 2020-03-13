83ad0ff1a30cc5790365a089b75afc2d8193ba77
kushiji <kushiji@protonmail.com>
2020-03-13 19:55:18 +0100

    Qutebrowser

--- config.h
+++ config.h
@@ -25,6 +25,7 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
     { "firefox",  			 NULL,       NULL,        1 << 1,     False,       0 },
+    { "qutebrowser",         NULL,       NULL,        1 << 1,     False,       0 },
 	{ "discord",           	 NULL,       NULL,        1 << 2,     False,       0 },
     { "spotify",             NULL,       NULL,        1 << 3,     False,       0 },    
 };
