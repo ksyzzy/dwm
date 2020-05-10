4b1e622ce954d120b0af26d11295a63765d57186
kushiji <kushiji@protonmail.com>
2020-05-10 14:58:38 +0200

    Updated config

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* Last Update: 25-01-2020 */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 30;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 50;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
